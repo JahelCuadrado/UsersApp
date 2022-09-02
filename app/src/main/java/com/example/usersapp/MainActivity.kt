75161121e5198e4d4d594a5154f61f1c26eb0b40
Jahel <jahelcuadradomanchado@hotmail.com>
2022-09-02 20:44:27 +0200

    Imagenes añadidas al recycler

--- app/src/main/java/com/example/usersapp/MainActivity.kt
+++ app/src/main/java/com/example/usersapp/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.usersapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.usersapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userAdapter         : UserAdapter
+    private lateinit var linearLayoutManager : RecyclerView.LayoutManager
+    private lateinit var binding             : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userAdapter = UserAdapter(this, getUsers())
+        linearLayoutManager = LinearLayoutManager(this)
+
+        binding.reciclerView.apply {
+            layoutManager = linearLayoutManager
+            adapter = userAdapter
+        }
+    }
+
+    private fun getUsers() : MutableList<User>{
+        val users = mutableListOf<User>()
+
+        val alain = User(1, "Alain", "Nicolás", "https://frogames.es/wp-content/uploads/2020/09/alain-1.jpg")
+        val samanta = User(2, "Samanta", "Meza", "https://upload.wikimedia.org/wikipedia/commons/b/b2/Samanta_villar.jpg")
+        val javier = User(3, "Javier", "Gómez", "https://live.staticflickr.com/974/42098804942_b9ce35b1c8_b.jpg")
+        val emma = User(4, "Emma", "Cruz", "https://upload.wikimedia.org/wikipedia/commons/d/d9/Emma_Wortelboer_%282018%29.jpg")
+
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(emma)
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(emma)
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(emma)
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(emma)
+
+        return users
     }
 }
