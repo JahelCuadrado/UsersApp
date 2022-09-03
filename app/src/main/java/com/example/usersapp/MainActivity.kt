91ea9b818535d8392d9ababeac391adb1de1c899
Jahel <jahelcuadradomanchado@hotmail.com>
2022-09-03 16:08:06 +0200

    implementado las getPreferences

--- app/src/main/java/com/example/usersapp/MainActivity.kt
+++ app/src/main/java/com/example/usersapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.usersapp
 
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.usersapp.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,17 +23,60 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        userAdapter = UserAdapter(this, getUsers()){ user, position -> //TODO
-            Toast.makeText(this, "${position+1}: " + user.name, Toast.LENGTH_SHORT).show();
+        initRecycler()
+        sharedPreferences()
+
+    }
+
+
+    //TODO
+    private fun sharedPreferences(){
+        val preferences = getPreferences(Context.MODE_PRIVATE)  //declaramos el modo de nuestro getpreference
+        val isFirstTime = preferences.getBoolean("first_time", true)  //recuperamos de las preferencias un valor booleano
+        // llamado first_name, este valor no existe en las prefencias, porque nos lo hemos inventado, asique devolverá el valor por defecto asignado
+
+
+
+        if (isFirstTime){
+            val dialogView = layoutInflater.inflate(R.layout.dialog_register, null)
+            MaterialAlertDialogBuilder(this)  //cuadro de dialogo personalizado
+                .setTitle("Bienvenido!!")
+                .setView(dialogView)
+                .setPositiveButton("Registrar", DialogInterface.OnClickListener { dialogInterface, i ->
+
+                    val username = dialogView.findViewById<TextInputEditText>(R.id.et_username).text.toString()  //guardamos en una variable el texto introducido
+
+                    with(preferences.edit()){
+                        putBoolean("first_time", false   ) //guardamos en la preference el valor false en first_time
+                        putString ("username"  , username) //guardamos en la preference el valor intrudicido, con la clave username.
+                            .apply()  //aplicar los cambios
+                    }
+                })
+                .setNegativeButton("Cancelar", null)
+                .show()
+        }else{
+            val username = preferences.getString("username", "Nose")
+            Toast.makeText(this, "Bienvenido $username", Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    private fun initRecycler(){
+        userAdapter = UserAdapter(this, getUsers(), ::eventClicRecycler)
         linearLayoutManager = LinearLayoutManager(this)
 
         binding.reciclerView.apply {
-            setHasFixedSize(true) //TODO
+            setHasFixedSize(true)
             layoutManager = linearLayoutManager
             adapter = userAdapter
         }
     }
+
+
+    private fun eventClicRecycler(user: User, position: Int){
+        Toast.makeText(this, "${position+1}: " + user.name, Toast.LENGTH_SHORT).show();
+    }
+
 
     private fun getUsers() : MutableList<User>{
         val users = mutableListOf<User>()
